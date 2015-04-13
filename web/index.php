cf88f8f1ecc303c344bd7cc74a5fa7f698f77178
flipdventures <ventures@flipdevelopment.ca>
2015-04-12 20:50:41 -0700

    Demo

--- web/index.php
+++ web/index.php
@@ -10,6 +10,17 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
+// Register the Twig templating engine
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/../views',
+));
+
+$app->get('/twig/{name}', function ($name) use ($app) {
+    return $app['twig']->render('index.twig', array(
+        'name' => $name,
+    ));
+});
+    
 // Our web handlers
 
 $app->get('/', function() use($app) {
