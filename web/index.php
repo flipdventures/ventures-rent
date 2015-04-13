83d0c067c1de52f130b852b6ab14db5f4d0519d8
flipdventures <ventures@flipdevelopment.ca>
2015-04-12 21:08:37 -0700

    added database access

--- web/index.php
+++ web/index.php
@@ -20,13 +20,37 @@ $app->get('/twig/{name}', function ($name) use ($app) {
         'name' => $name,
     ));
 });
-    
+   
+    $dbopts = parse_url(getenv('DATABASE_URL'));
+    $app->register(new Herrera\Pdo\PdoServiceProvider(),
+                   array(
+                         'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+                         'pdo.port' => $dbopts["port"],
+                         'pdo.username' => $dbopts["user"],
+                         'pdo.password' => $dbopts["pass"]
+                         )
+                   );
 // Our web handlers
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return str_repeat('Hello', getenv('TIMES'));
+  return 'Hello Kayleigh';
 });
+    
+    $app->get('/db/', function() use($app) {
+              $st = $app['pdo']->prepare('SELECT name FROM test_table');
+              $st->execute();
+              
+              $names = array();
+              while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+              $app['monolog']->addDebug('Row ' . $row['name']);
+              $names[] = $row;
+              }
+              
+              return $app['twig']->render('database.twig', array(
+                                                                 'names' => $names
+                                                                 ));
+              });
 
 $app->run();
 
